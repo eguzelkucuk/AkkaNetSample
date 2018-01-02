90010bbc1aa05d105e035e03c8e64c94bedfc054
Erkan Güzelküçük <erkan.guzelkucuk@albil.com.tr>
2018-01-02 18:47:05 +0300

    First Commit

---
+++ OnlineOrdering.Common/Actors/ProcessOrder.cs
@@ -0,0 +1,17 @@
+﻿using Akka.Actor;
+using OnlineOrdering.Common.Messages;
+using System;
+
+namespace OnlineOrdering.Common.Actors
+{
+    /// <summary>
+    /// Responsible for processing order
+    /// </summary>
+    public class ProcessOrder : ReceiveActor
+    {
+        public ProcessOrder()
+        {
+            Receive<Order>(m => Console.WriteLine("Order received for processing. {0}", m));
+        }
+    }
+}
---
+++ PlaceOrder.WebAPI/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace PlaceOrder.WebAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        public static ActorSystem OrderPlacingActorSystem;
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            OrderPlacingActorSystem = ActorSystem.Create("OrderPlacingActorSystem");
+        }
+    }
+}
